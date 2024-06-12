277c8f028679a4183f047e490db52d9676e30ef3
Shakejelly <fredrich93@gmail.com>
2024-06-12 14:43:59 +0200

    igen..

--- src/components/Cv.jsx
+++ src/components/Cv.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import cvData from '...data/cvdata.json'
+import cvData from '.../data/cvdata.json'
 
 const Cv = () => {
     const [cvInfo, setCvInfo] = useState({ education: [], work: [] });
