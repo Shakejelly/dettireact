dc2348ecc09166bcc2eb8fde8bcec2fcb9c835d6
Fredrich Benedetti <fredrich93@gmail.com>
2024-06-19 20:22:54 +0200

    asd

--- src/components/Cv.jsx
+++ src/components/Cv.jsx
@@ -22,49 +22,48 @@ const Cv = () => {
     fetchCvData();
   }, []);
 
-  
-    if (loading) {
-      return <div>Laddar CV...</div>;
-    }
-  
-    if (error) {
-      return <div>{error}</div>;
-    }
-  
-    return (
-        <div className='cv-wrapper'>
-          <div className='section'>
-            <h2>Utbildning</h2>
-            {cvInfo.education && cvInfo.education.length > 0 ? (
-              cvInfo.education.map((edu, index) => (
-                <div key={index} className="education-item">
-                  <h3>{edu.schoolName}</h3>
-                  <p>{edu.timeLapse}</p>
-                  <p>{edu.text}</p>
-                </div>
-              ))
-            ) : (
-              <p>Ingen utbildning tillgänglig</p>
-            )}
-          </div>
-    
-          <div className='section'>
-            <h2>Arbetslivserfarenhet</h2>
-            {cvInfo.work && cvInfo.work.length > 0 ? (
-              cvInfo.work.map((work, index) => (
-                <div key={index} className="work-item">
-                  <h3>{work.companyName}</h3>
-                  <p>{work.timeLapse}</p>
-                  <p>{work.role}</p>
-                  <p>{work.text}</p>
-                </div>
-              ))
-            ) : (
-              <p>Ingen arbetslivserfarenhet tillgänglig</p>
-            )}
-          </div>
-        </div>
-      );
-    };
-    
-    export default Cv;
+  if (loading) {
+    return <div>Laddar CV...</div>;
+  }
+
+  if (error) {
+    return <div>{error}</div>;
+  }
+
+  return (
+    <div className='cv-wrapper'>
+      <div className='section'>
+        <h2>Utbildning</h2>
+        {cvInfo.education && cvInfo.education.length > 0 ? (
+          cvInfo.education.map((edu, index) => (
+            <div key={index} className="education-item">
+              <h3>{edu.schoolName}</h3>
+              <p>{edu.timeLapse}</p>
+              <p>{edu.text}</p>
+            </div>
+          ))
+        ) : (
+          <p>Ingen utbildning tillgänglig</p>
+        )}
+      </div>
+
+      <div className='section'>
+        <h2>Arbetslivserfarenhet</h2>
+        {cvInfo.work && cvInfo.work.length > 0 ? (
+          cvInfo.work.map((work, index) => (
+            <div key={index} className="work-item">
+              <h3>{work.companyName}</h3>
+              <p>{work.timeLapse}</p>
+              <p>{work.role}</p>
+              <p>{work.text}</p>
+            </div>
+          ))
+        ) : (
+          <p>Ingen arbetslivserfarenhet tillgänglig</p>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Cv;
